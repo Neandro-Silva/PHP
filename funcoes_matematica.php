f397d25716c90938ff695c5cfc62b65de5591864
Neandro <neandrosilva.11@gmail.com>
2023-03-16 22:48:40 -0300

    funções nativas p/ tarefas matemáticas

---
+++ funcoes_matematica.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    
+    <?php
+       
+       //$num = 7.3;
+
+       //$num = -2.45; //-2
+       //echo ceil($num); //arredonda p/ cima
+       //$num = -2.45; //-3
+       //echo round($num); //arredonda com base na fração ->$num = 7.3; .0 e .4 => baixo / .5 => cima
+       //echo rand(); //gerar um valor aleatório - 0 até randmax
+       //echo rand(10, 20); //defenindo o intervalo
+       //echo '<br/> '. getrandmax();
+       echo sqrt(10) //raiz quadrada
+    ?>
+
+</body>
+</html>
