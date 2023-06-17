aeba2d8a9d89a1f7d3dd4bcd2e2559fdef5aa3ef
Neandro <neandrosilva.11@gmail.com>
2023-06-17 09:05:39 -0300

    exercicio - mudança no código

--- app_help_desk/consultar_chamado.php
+++ app_help_desk/consultar_chamado.php
@@ -1,26 +1,43 @@
-<?php
-  require_once "validador_acesso.php";
-?>
+<?php require_once "validador_acesso.php" ?>
 
 <?php
 
   //chamados
   $chamados = array();
 
-  //abrir arquivo.hd
+  echo $_SESSION['perfil_id'];
+
+  //abrir o arquivo.hd
   $arquivo = fopen('arquivo.hd', 'r');
 
-
-  //enquanto houver registros (linhas) a serem recuperadas 
+  //enquanto houver registros (linhas) a serem recuperados
   while(!feof($arquivo)) { //testa pelo fim de um arquivo
-     //linhas
+    //linhas  
     $registro = fgets($arquivo);
-    $chamados[] = $registro;
+
+    //explode dos detalhes do registro para verificar o id do usuário responsável pelo cadastro
+    $registro_detalhes = explode('#', $registro);
+
+    //(perfil id = 2) só vamos exibir o chamado, se ele foi criado pelo usuário
+    if($_SESSION['perfil_id'] == 2) {
+
+      //se usuário autenticado não for o usuário de abertura do chamado então não faz nada
+      if($_SESSION['id'] != $registro_detalhes[0]) {
+        continue; //não faz nada
+
+      } else {
+        $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+      }
+
+    } else {
+      $chamados[] = $registro; //adiciona o registro do arquivo ao array $chamados
+    }
+
   }
 
   //fechar o arquivo aberto
   fclose($arquivo);
-
+  
 ?>
 
 <html>
@@ -48,7 +65,7 @@
       </a>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a href="logoff.php" class="nav-link">SAIR</a>
+          <a class="nav-link" href="logoff.php">SAIR</a>
         </li>
       </ul>
     </nav>
@@ -64,39 +81,32 @@
             
             <div class="card-body">
 
-            <?php foreach($chamados as $chamado) { ?>
+              <?php foreach($chamados as $chamado) { ?>
               
-              <?php
-                $chamado_dados = explode('#', $chamado);
+                <?php
 
-                //
-                if($_SESSION['perfil_id'] == 2) {
-                  //só vamos exibir o chamdo, se ele foi criado pelo usuário
-                  if($_SESSION['id'] != $chamado_dados[0]) {
+                  $chamado_dados = explode('#', $chamado);
+
+					        //não existe detalhes do chamado se ele não estiver completo
+                  if(count($chamado_dados) < 3) {
                     continue;
                   }
 
-                }
+                ?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?=$chamado_dados[1]?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
+                    <p class="card-text"><?=$chamado_dados[3]?></p>
 
-                if(count($chamado_dados) < 3) {
-                  continue;
-                }
-              ?>
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title"><?=$chamado_dados[1]?></h5>
-                  <h6 class="card-subtitle mb-2 text-muted"><?=$chamado_dados[2]?></h6>
-                  <p class="card-text"><?=$chamado_dados[3]?></p>
-
+                  </div>
                 </div>
-              </div>
 
               <?php } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
-                  <a href="home.php"class="btn btn-lg btn-warning btn-block">Voltar</a>
+                  <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
                 </div>
               </div>
             </div>
